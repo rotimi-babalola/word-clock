49e5e3266b08ff7655b8e8591dab7657682317c3
rotimi-babalola <rotimi.babalola@andela.com>
2018-12-28 10:56:55 +0100

    initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const webpackMerge = require('webpack-merge');
+const commonConfig = require('./webpack.common');
+
+module.exports = (env) => {
+  /* eslint global-require: 0 */
+  /* eslint import/no-dynamic-require: 0 */
+  const envConfig = require(`./build-utils/webpack.${env.mode}`);
+
+  console.log(env);
+  return webpackMerge({ mode: env.mode }, commonConfig, envConfig);
+};
