6bd1dfdbd7fa0f0f7f4e6cb8eebbdecc4ea889fc
rotimi-babalola <rotimi.babalola@andela.com>
2018-12-28 09:27:50 +0100

    configure eslint

---
+++ constants.js
@@ -0,0 +1,9 @@
+const AM = 'am';
+const PM = 'pm';
+const MINUTES_IN_AN_HOUR = 60;
+
+module.exports = {
+  AM,
+  PM,
+  MINUTES_IN_AN_HOUR,
+};
--- humanizeTime.js
+++ humanizeTime.js
@@ -1,15 +1,11 @@
 const { toWords } = require('number-to-words');
-
-const AM = 'am';
-const PM = 'pm';
-const MINUTES_IN_AN_HOUR = 60;
+const { MINUTES_IN_AN_HOUR } = require('./constants');
 
 function humanizeTime(time) {
-  const re = /(AM|PM)/gi;
+  // const re = /(AM|PM)/gi;
   const digitRE = /\d+/g;
 
-  const partOfDay = time.match(re)[0];
-  // string that will contain the final time
+  // const partOfDay = time.match(re)[0];
 
   let [hours, minutes] = time.match(digitRE);
 
@@ -19,21 +15,24 @@ function humanizeTime(time) {
 
   if (minutes === 0) {
     return `${toWords(hours).toUpperCase()} O'CLOCK`;
-  } else if (minutes === 15) {
-    return `QUARTER PAST ${toWords(hours).toUpperCase()}`;
-  } else if (minutes === 30) {
-    return `HALF PAST ${toWords(hours).toUpperCase()}`;
-  } else if (minutes === 45) {
-    return `QUARTER TO ${toWords(hours + 1).toUpperCase()}`;
-  } else if (minutes < 30) {
-    return `${toWords(minutes).toUpperCase()} MINUTES PAST ${toWords(
-      hours
-    ).toUpperCase()}`;
-  } else {
-    return `${toWords(
-      MINUTES_IN_AN_HOUR - minutes
-    ).toUpperCase()} MINUTES TO ${toWords(hours + 1).toUpperCase()}`;
   }
+  if (minutes === 15) {
+    return `QUARTER PAST ${toWords(hours).toUpperCase()}`;
+  }
+  if (minutes === 30) {
+    return `HALF PAST ${toWords(hours).toUpperCase()}`;
+  }
+  if (minutes === 45) {
+    return `QUARTER TO ${toWords(hours + 1).toUpperCase()}`;
+  }
+  if (minutes < 30) {
+    return `${toWords(minutes).toUpperCase()} MINUTES PAST ${toWords(
+      hours,
+    ).toUpperCase()}`;
+  }
+  return `${toWords(
+    MINUTES_IN_AN_HOUR - minutes,
+  ).toUpperCase()} MINUTES TO ${toWords(hours + 1).toUpperCase()}`;
 }
 
 console.log(humanizeTime('7:00PM'));
